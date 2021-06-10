bbbd77fe94fbc55157a544c7f2a6e32c91572af2
gunnner <ctrlgrand@gmail.com>
2021-06-10 11:43:23 +0000

    add error handlers to api

--- app/api/rails_task/api.rb
+++ app/api/rails_task/api.rb
@@ -4,6 +4,18 @@ module RailsTask
     format :json
     prefix :api
 
+    rescue_from JWT::DecodeError do
+      error!(error: { code:401, message: 'invalid token' })
+    end
+
+    rescue_from Grape::Exceptions::MethodNotAllowed do
+      error!(error: { code: 405, message: 'not allowed' })
+    end
+
+    rescue_from Grape::Exceptions::ValidationErrors do |e|
+      error!(error: { code: 400, message: e.message })
+    end
+
     mount RailsTask::V1::Albums
     mount RailsTask::V1::Photos
     mount RailsTask::V1::Login
